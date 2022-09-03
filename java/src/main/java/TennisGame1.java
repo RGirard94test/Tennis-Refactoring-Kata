fe7a23e7171bf39405db2933012839bfc0c60006
Simon Masliah <simon.masliah@octo.com>
2022-09-03 14:38:12 +0200

    splitted methods for getScore

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -4,16 +4,16 @@ public class TennisGame1 implements TennisGame {
     private int m_score1 = 0;
     private int m_score2 = 0;
 
-    private String player1Name;
-    private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGame1(Player player1, Player player2) {
-        this.player1Name = player1.name;
-        this.player2Name = player2.name;
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == player1Name)
+        if (playerName == player1.name)
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -21,56 +21,67 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
         if (m_score1==m_score2)
+            score = computeScoreWhenDraw();
+        else if (m_score1>=4 || m_score2>=4)
+            score = computeScoreWhenOverForty();
+        else
+            score = computeScoreRegularPlay(score);
+        return score;
+    }
+
+    private String computeScoreRegularPlay(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
         {
-            switch (m_score1)
+            if (i==1) tempScore = m_score1;
+            else { score +="-"; tempScore = m_score2;}
+            switch(tempScore)
             {
                 case 0:
-                        score = "Love-All";
+                    score +="Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score +="Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score +="Thirty";
                     break;
-                default:
-                        score = "Deuce";
+                case 3:
+                    score +="Forty";
                     break;
-                
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        return score;
+    }
+
+    private String computeScoreWhenOverForty() {
+        String score;
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage "+ player1.name;
+        else if (minusResult ==-1) score ="Advantage " + player2.name;
+        else if (minusResult>=2) score = "Win for "+ player1.name;
+        else score ="Win for " + player2.name;
+        return score;
+    }
+
+    private String computeScoreWhenDraw() {
+        String score;
+        switch (m_score1)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage "+ player1Name;
-            else if (minusResult ==-1) score ="Advantage " + player2Name;
-            else if (minusResult>=2) score = "Win for "+ player1Name;
-            else score ="Win for " + player2Name;
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
         }
         return score;
     }

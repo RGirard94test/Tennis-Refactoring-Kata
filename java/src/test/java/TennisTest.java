cc36914a77cf2292c836d90322fd4e4e79fa5a04
Simon Masliah <simon.masliah@octo.com>
2022-09-03 14:05:45 +0200

    first commit

--- java/src/test/java/TennisTest.java
+++ java/src/test/java/TennisTest.java
@@ -81,23 +81,4 @@ public class TennisTest {
         TennisGame1 game = new TennisGame1("player1", "player2");
         checkAllScores(game);
     }
-
-    @Test
-    public void checkAllScoresTennisGame2() {
-        TennisGame2 game = new TennisGame2("player1", "player2");
-        checkAllScores(game);
-    }
-
-    @Test
-    public void checkAllScoresTennisGame3() {
-        TennisGame3 game = new TennisGame3("player1", "player2");
-        checkAllScores(game);
-    }
-
-    @Test
-    public void checkAllScoresTennisGame4() {
-        TennisGame game = new TennisGame4("player1", "player2");
-        checkAllScores(game);
-    }
-
 }
